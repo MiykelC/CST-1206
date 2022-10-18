bd51fb01d044a78e211cc03b0d8461ba57e3e6d3
MiykelC <105033752+MiykelC@users.noreply.github.com>
2022-10-18 12:18:43 -0700

    .

---
+++ EJS/app.js
@@ -0,0 +1,13 @@
+const express =require('require');
+const app = express();
+const PORT = 5000;
+
+app.set("view engine", "ejs")
+
+app.get("/", (req,res)=>{
+  res.render("index", {name: "Michael"})
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
