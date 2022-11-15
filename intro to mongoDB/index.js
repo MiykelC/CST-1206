1481bddc41408fb10c50779906928a9e89d05e4d
MiykelC <105033752+MiykelC@users.noreply.github.com>
2022-11-15 09:17:35 -0800

    Update index.js

--- intro to mongoDB/index.js
+++ intro to mongoDB/index.js
@@ -2,50 +2,80 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+// User Model coming from models
 const User = require("./models/user");
-require(`dotenv`).config();
 
-
+// So we can accept json information
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, (error) => {
-    if(error){
-        console.error(`there is an error ${error}`);
+
+mongoose.connect(process.env.MONGO_URI, (error) => {
+    if (error) {
+        console.error(`There was an error : ${error}` );
     } else {
-        console.log("connected to mongoDB");
+        console.log("Connected to the database");
     }
 })
 
-
 app.get("/", (req, res) => {
-    res.send("USER ENDPOINTS")
-});
+    res.send("USER ENDPOINTS!");
+})
 
 app.get("/users", (req, res) => {
-        User.find().then((users) => {
-        return res.status(200).json(users);
+    // returns async response ; // This returns a promise
+    // then is waiting for the response
+    // User helps me get hold of information from mongodb atlas server
+    User.find().then((data) => {
+        return res.status(200).json({
+            message: "Succesfully fetched the user",
+            data
+        }); 
+    }).catch((error) => {
+        return res.status(404).json({
+            message: "Error",
+            error
+        }); 
     })
-});
+})
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
     let newUser = {
-        name:req.body.name,
-        email:req.body.email ,  
-        password:req.body.password,
-        phoneNumber:req.body.phoneNumber
+       name: req.body.name,
+       email: req.body.email,
+       password: req.body.password,
+       phoneNumber: req.body.phoneNumber
     }
-    // we are making an instance of user model
+
+    // We are making an instance of User Model
     const user = new User(newUser);
 
-    user.save().then((data) => {
-        return res.status(201).json({
-                message: "user created successfully",
-                data
-            })
-    })
-})   
+    // Save method helps you save data in database
+    // user.save().then((data) => {
+    //     return res.status(201).json({
+    //         message: "User succesfully created",
+    //         data
+    //     })
+    // })
+
+    try {
+
+    let response = await  user.save();
+    return res.status(201).json({
+            message: "User succesfully created",
+            response
+        })
+    } catch( error) {
+        res.status(500).json({
+            message: "Error creating user",
+            error
+        })
+    }
 
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Running on PORT ${PORT}`);
+})
